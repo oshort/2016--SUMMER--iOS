24f86cdd2fef77975506a5415e7437bf4f17ff7b
Ben Gohlke <jcgohlke@me.com>
2016-06-28 10:28:04 -0400

    Initial commit for AgeCalculator

---
+++ AgeCalculator/AgeCalculator/AgeViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AgeCalculator
+//
+//  Created by Ben Gohlke on 6/28/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AgeViewController : UIViewController
+
+
+@end
+
---
+++ AgeCalculator/AgeCalculator/AgeViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AgeCalculator
+//
+//  Created by Ben Gohlke on 6/28/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import "AgeViewController.h"
+
+@interface AgeViewController ()
+
+@end
+
+@implementation AgeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
